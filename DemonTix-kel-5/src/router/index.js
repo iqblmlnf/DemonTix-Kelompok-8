16551b3f78e8ba0b596d43631885e298a18045e0
fatapureka56 <fatapureka56@students.amikom.ac.id>
2025-05-23 07:31:54 +0700

    HubungiKami

--- DemonTix-kel-5/src/router/index.js
+++ DemonTix-kel-5/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 import Jelajah from '@/views/Jelajah.vue';
+import HubungiKami from '@/views/HubungiKami.vue'
 import SyaratKetentuan from '@/views/Syarat&Ketentuan.vue';
 import Faq from '@/views/Faq.vue';
 import Login from '@/views/Login.vue';
@@ -10,6 +11,7 @@ import Login from '@/views/Login.vue';
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/jelajah', name: 'Jelajah', component: Jelajah },
+    { path: '/hubungi-kami', name: 'HubungiKami', component: HubungiKami },
   { path: '/Detail/:id', name: 'Detail', component: () => import('@/views/Detail.vue'), props: true }, // Route dinamis
   { path: '/beli/:id', name: 'BeliTiket', component: () => import('@/views/BeliTiket.vue'), props: true},
   { path: '/syarat-ketentuan', name: 'SyaratKetentuan', component: SyaratKetentuan },
