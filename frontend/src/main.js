77d97a5c96eeb7057f72a7f1a4a994ad38c21b99
fatapureka56 <fatapureka56@students.amikom.ac.id>
2025-07-31 02:57:39 +0700

    update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,15 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router'; 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import router from './router';
 
+// ✅ CSS Libraries
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'admin-lte/dist/css/adminlte.min.css';
+
+// ✅ JS Libraries (harus urut)
+import 'jquery';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'admin-lte/dist/js/adminlte.min.js';
 
 createApp(App).use(router).mount('#app');
