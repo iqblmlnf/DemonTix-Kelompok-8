ec38ed10900304f887bb8ed5d99f7a6bf11634fa
TegarSaputra166 <tegarsaputra20@students.amikom.ac.id>
2025-07-31 02:24:25 +0700

    Update search / app.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,51 +1,29 @@
 <template>
   <div>
-    <Navbar v-if="!isLoginPage" @search="handleSearch" @clear-search="clearResults" />
-
-    <SearchResult v-if="searchQuery" :searchQuery="searchQuery" :events="filteredEvents" />
-    <router-view v-else />
-    
-    <Footer v-if="!isLoginPage" />
+    <Navbar
+      v-if="!isMinimalPage"
+    />
+    <router-view />
+    <Footer v-if="!isMinimalPage" />
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
 import Footer from "@/components/Footer.vue";
-import SearchResult from "@/components/SearchResult.vue";
-import { allEvents } from "@/data/events.js";
 
 export default {
   name: "App",
   components: {
     Navbar,
     Footer,
-    SearchResult
-  },
-  data() {
-    return {
-      searchQuery: "",
-      events: allEvents
-    };
   },
   computed: {
-  filteredEvents() {
-    if (!this.searchQuery) return [];
-    return this.events.filter(event =>
-      event.title.toLowerCase().includes(this.searchQuery.toLowerCase())
-    );
-  },
-  isLoginPage() {
-    return this.$route.path === "/login";
-  }
-},
-  methods: {
-    handleSearch(query) {
-      this.searchQuery = query;
+    isMinimalPage() {
+      const path = this.$route.path;
+      return path === "/login" || path.startsWith("/admin");
     },
-    clearResults() {
-      this.searchQuery = "";
-    }
-  }
+  },
 };
 </script>
+
