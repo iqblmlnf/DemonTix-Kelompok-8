bcc1d86c51ffa3d9539fca7428cc827cb476f380
iqblmlnf <iqbalmaulanafajar36@gmail.com>
2025-07-31 01:11:00 +0700

    add InvoiceController.php di web.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InvoiceController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/invoice/pdf/{id}', [InvoiceController::class, 'cetak']);
